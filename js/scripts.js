e431a2eade803166c91ba82db52aacfe610bef2b
yaroslavpolubiedov <y.polubiedov@studytube.nl>
2016-07-27 16:25:34 +0300

    added new background for body

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-var a = document.getElementsByTagName('a');
-
-
 function addClass(elem) {
+    var a = document.getElementsByTagName('a');
     for (i = 0; i < a.length; i++) {
         a[i].classList.remove('active');
     }
     elem.classList.add('active');
+    return document.getElementById(elem);
 };
+var name = addClass();
